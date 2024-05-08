f4679e705216d6596e14299523218c0c0eeb2ce3
Guillermina Fara <marfarsan@alu.edu.gva.es>
2024-05-08 16:02:56 +0200

    h

---
+++ tests/test_multipllica.py
@@ -0,0 +1,15 @@
+# Librería pytest para ejecutar tests
+import pytest
+
+# Importar archivo de ejercicio
+from ejercicios.operaciones import multiplica
+
+# Clase para crear tests. Las funciones de testeo deberán crearse en esta clase
+class multiplicaTestClass:
+
+    # Test para la operación suma
+    def test_multiplica(self):
+        assert multiplica(4,5) == 20
+        assert multiplica(-1,-2) == 2
+        assert multiplica(-7,5) == -35
+        assert multiplica(-7,9) == -63
